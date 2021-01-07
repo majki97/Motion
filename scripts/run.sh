e6d809ed489b86e1b41fdf2a0c6274a87a384232
majki97 <mwyszowski@gmail.com>
2021-01-07 17:01:34 +0000

    onemore

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
 python manage.py collectstatic --no-input
-rm -rf /frontend/build/* && -r ?frontend_tmp/* /frontend
+rm -rf /frontend/build/* && cp -r /frontend_tmp/* /frontend
 gunicorn -w 4 -b 0.0.0.0:8000 project.wsgi:application
