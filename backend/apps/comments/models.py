53e5c9ab6101cc2dbebc2331742ca2c33fa3a8c6
MiLu <michelle.lundak@yahoo.com>
2021-01-13 13:55:42 +0100

    added comment app

---
+++ backend/apps/comments/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from apps.post import Post
+
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=300)
+    author = models.CharField(max_length=80)
+    created = models.DateField(auto_now_add=True)
+    approved_comment = models.BooleanField(default=False)
+    post = models.ForeignKey(to=Post, related_name='comments', on_delete=models.SET_NULL)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return f'Comment {self.text} by {self.author}'
