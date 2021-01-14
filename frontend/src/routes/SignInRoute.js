7ed0e792a78e251f37674d2f85a4029ae6d0f0d2
MiLu <michelle.lundak@yahoo.com>
2021-01-14 16:21:40 +0100

    designed congrats page

--- frontend/src/routes/SignInRoute.js
+++ frontend/src/routes/SignInRoute.js
@@ -1,9 +1,10 @@
-
+import { Link } from 'react-router-dom';
 import  SignIn  from "../components/SignInPage";
 
 export const SignInRoute = () => {
     return (
         <div>
+            <Link to="/Congrats">Congrats</Link>
             <SignIn />
         </div>
     );
